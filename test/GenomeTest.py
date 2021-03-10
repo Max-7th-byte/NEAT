6a8e890c2287d8c817db3d06741d6ece1d7f4dad
Max-7th-byte <max.dubakov@gmail.com>
2021-03-10 19:05:35 +0100

    Visual representation added

--- test/GenomeTest.py
+++ test/GenomeTest.py
@@ -5,10 +5,6 @@ from genome.Genome import Genome
 class GenomeTest(unittest.TestCase):
 
     def test_genome(self):
-        genome_1 = Genome(0, 3, 2)
+        genome_1 = Genome(generation_number=1, input_nodes=2, output_nodes=3)
         genome_2 = Genome(0, 3, 2)
-        for con in genome_1.connections():
-            print(con)
 
-        for node in genome_1.nodes():
-            print(node)
